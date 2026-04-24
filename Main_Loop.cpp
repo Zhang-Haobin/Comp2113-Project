652bdb45ca7d8d8f3540e28f8e83e2da546de1b7
ZFSR <139683209+ZFSR@users.noreply.github.com>
2026-04-24 11:02:51 +0800

    Update Main_Loop.cpp

--- Main_Loop.cpp
+++ Main_Loop.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+using nameaspace std;
 
+int main(){
+bool game_quit = True
+int select = 0
+while (game_quit){
+cout<< "SPIRE LITE" <<endl;
+cout<< "1-Game Start" <<endl;
+cout<< "2-Options" <<endl;
+cout<< "3-Quit" <<endl;
+cout<< "enter the numbers before the entries for selecting" <<endl;
+cin>>select;
+if (
+
+
+
+
+
+
+}
+
+}
