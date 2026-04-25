027bc88ebbdd53e59d41a194085876097547db20
Zhang Haobin <zhanghaobin2007@gmail.com>
2026-04-25 23:42:15 +0800

    Update Main_Loop.cpp

--- Main_Loop.cpp
+++ Main_Loop.cpp
@@ -2,17 +2,17 @@
 #include <string>
 #include <fstream>
 #include <iomanip>
-using nameaspace std;
+using namespace std;
 
 int main(){
-bool game_quit = True, option_quit = True, difficulty_quit = True;
+bool game_quit = true, option_quit = true, difficulty_quit = true;
 int select_menu = 0, select_option=0, select_difficulty=0;
 int difficulty = 2
   
   
   
 while (game_quit){                                                   //一级菜单  
-game_quit = True
+game_quit = true;
 cout<< "SPIRE LITE" <<endl;
 cout<< "1-Game Start" <<endl;
 cout<< "2-Options" <<endl;
@@ -20,17 +20,17 @@ cout<< "3-Quit" <<endl;
 cout<< "enter the numbers before the entries for selecting" <<endl;
 cin>>select_menu;
 if ( select_menu == "3"){                                                   //退出游戏
-    game_quit = False
+    game_quit = false;
 }
 elif ( select_menu == "2"){                                                     
-    option_quit = True;
+    option_quit = true;
     while ( option_quit ) {                                              //二级菜单-设置
         cout<<"1-Change Difficulty"<<endl; 
         cout<<"2-Return to Menu"<<endl;
         cout<< "enter the numbers before the entries for selecting" <<endl;
         cin>>select_option;
         if ( select_option == "1" ){
-            difficulty_quit = True;
+            difficulty_quit = true;
             while ( difficulty_quit ){                                   //三级菜单-难度
             cout<<"1-Journey"<<endl;
             cout<<"2-Classic (Default)"<<endl;
@@ -73,7 +73,7 @@ elif ( select_menu == "2"){
             }
         }  
         elif ( select == "2" ){                                           //退出设置
-            option_quit = False;
+            option_quit = false;
             select_menu = 0;
         }
         else{
