32ea4bb8140aceb69f9acfd7b2a6c85b4cdec0cf
Zhang Haobin <zhanghaobin2007@gmail.com>
2026-04-26 16:11:45 +0800

    up

--- Main_Loop.cpp
+++ Main_Loop.cpp
@@ -5,37 +5,37 @@
 using namespace std;
 
 int main(){
-bool game_quit = true, option_quit = true, difficulty_quit = true;
-int select_menu = 0, select_option=0, select_difficulty=0;
-int difficulty = 2
+    bool game_quit = true, option_quit = true, difficulty_quit = true;
+    int select_menu = 0, select_option=0, select_difficulty=0;
+    int difficulty = 2;
   
   
   
 while (game_quit){                                                   //一级菜单  
-game_quit = true;
-cout<< "SPIRE LITE" <<endl;
-cout<< "1-Game Start" <<endl;
-cout<< "2-Options" <<endl;
-cout<< "3-Quit" <<endl;
-cout<< "enter the numbers before the entries for selecting" <<endl;
-cin>>select_menu;
-if ( select_menu == "3"){                                                   //退出游戏
-    game_quit = false;
-}
-elif ( select_menu == "2"){                                                     
+    game_quit = true;
+    cout<< "SPIRE LITE" <<endl;
+    cout<< "1-Game Start" <<endl;
+    cout<< "2-Options" <<endl;
+    cout<< "3-Quit" <<endl;
+    cout<< "enter the numbers before the entries for selecting" <<endl;
+    cin>>select_menu;
+    if ( select_menu == 3){                                                   //退出游戏
+        game_quit = false;
+    }
+else if ( select_menu == 2){                                                     
     option_quit = true;
     while ( option_quit ) {                                              //二级菜单-设置
         cout<<"1-Change Difficulty"<<endl; 
         cout<<"2-Return to Menu"<<endl;
         cout<< "enter the numbers before the entries for selecting" <<endl;
         cin>>select_option;
-        if ( select_option == "1" ){
+        if ( select_option == 1 ){
             difficulty_quit = true;
             while ( difficulty_quit ){                                   //三级菜单-难度
             cout<<"1-Journey"<<endl;
             cout<<"2-Classic (Default)"<<endl;
             cout<<"3-Hardcore"<<endl;
-            cout<<"4-Return to Options"
+            cout<<"4-Return to Options"<<endl;
                 switch (difficulty) {
                     case 1:
                     cout<<"current difficulty: Journey"<<endl;
@@ -45,44 +45,45 @@ elif ( select_menu == "2"){
                     break;
                     case 3:
                     cout<<"current difficulty: Hardcore"<<endl;
-                    break
+                    break;
                 }
             cout<<"enter the numbers before the entries for changing difficulty" <<endl;
             cin>>select_difficulty;
                 switch (select_difficulty){
                     case 1:
                         difficulty = 1;
-                        cout<<"You have set the difficulty to Journey"
-                        break
+                        cout<<"You have set the difficulty to Journey"<<endl;
+                        break;
                     case 2:
                         difficulty = 2;
-                        cout<<"You have set the difficulty to Classic"
-                        break
+                        cout<<"You have set the difficulty to Classic"<<endl;
+                        break;
                     case 3:
                         difficulty = 3;
-                        cout<<"You have set the difficulty to Hardcore"
-                        break
+                        cout<<"You have set the difficulty to Hardcore"<<endl;
+                        break;
                     case 4:                                                          //退出难度
-                        difficulty_quit = False
-                        select_option = 0
+                        difficulty_quit = false;
+                        select_option = 0;
+                        break;
                     default:
                         cout<<"please enter a number in range (1-4)"<<endl;
-                        break
+                        break;
                         
                 }
             }
         }  
-        elif ( select == "2" ){                                           //退出设置
+        else if ( select_option == 2 ){                                           //退出设置
             option_quit = false;
             select_menu = 0;
         }
         else{
-            cout<< cout<<"please enter a number in range (1-2) <<endl;
+            cout<<"please enter a number in range (1-2)"<<endl;
         }
     }
 }
 else{
-    cout<<"please enter a number in range (1-3) <<endl;
+    cout<<"please enter a number in range (1-3)"<<endl;
 }
   
 
