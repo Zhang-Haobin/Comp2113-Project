5c5ca5d95bc5ba81e3ced611ffb447e8885ad9e1
ZFSR <139683209+ZFSR@users.noreply.github.com>
2026-04-28 15:35:06 +0800

    Update Main_Loop.cpp

--- Main_Loop.cpp
+++ Main_Loop.cpp
@@ -1,67 +1,87 @@
 #include <iostream>
-#include <stdio.h>
 #include <string>
 #include <fstream>
 #include <iomanip>
 #include "player.h"
+#include "save.h"
 
 using namespace std;
 
-enum class Screen { welcome, lobby, battle, paused, end };
-enum class Menu_page { save_slot, record, info }
-enum class Difficulty { easy, normal, hard }; // todo: add maybe one more
+enum class Screen { welcome, lobby, battle, paused, end };                  
+enum class Difficulty { easy, normal, hard, lunatic }; 
 
 bool is_game_running = true;
 int difficulty = 2;
 Screen cur_screen = Screen::welcome;
 
-void welcomeprocess();
+void welcomeprocess();                              
 void lobbyprocess();
 void battleprocess();
 void pausedprocess();
 
+void save_slotprocess();
+void recordprocess();
+void infoprocess();
+
 int main() {
 
-    // todo: use something like while(getc()) {} to read keyboard
-    //       then the player can skip pressing Enter every time
+while(is_game_running) {
+    switch(cur_screen){
+        case Screen::welcome:
+            welcomeprocess();
+            break;
+        case Screen::lobby:
+            lobbyprocess();
+            break;
+        case Screen::battle:
+            battleprocess();
+            break;
+        case Screen::paused:
+            pausedprocess();
+            break;
+        case Screen::end:
+            is_game_running = false;
+            break;
+    }
 
-    while(is_game_running) {
-        switch(cur_screen){
-            case Screen::welcome:
-                welcomeprocess();
-                break;
-            case Screen::lobby:
-                lobbyprocess();
-                break
-            case Screen::battle:
-                battleprocess();
-                break
-            case Screen::paused:
-                pausedprocess();
-                break
-            case Screen::end:
-                is_game_running = false
-                break;
-        }
+return 0;
+}
 
+//this is welcome page
 void welcomeprocess(){
-    cout<<"\n===|Spire Lite|===\n"<<endl;
-    cout<<"    Main Menu   "<<endl;
-    cout<<"   1-Start      "<<endl;
-    cout<<"   2-Game Record"<<endl;
-    cout<<"   3-More Info  "<<endl;
-    cout<<"   4-Quit       "<<endl;
 
-    
+cout<<"\n===|Spire Lite|===\n"<<endl;
+cout<<"    Main Menu   "<<endl;
+cout<<"   1-Start      "<<endl;
+cout<<"   2-Game Record"<<endl;
+cout<<"   3-More Info  "<<endl;
+cout<<"   4-Quit       "<<endl;
 
-
-
+int choice;
+cin>>choice;
+switch (choice){
+    case 1:
+        save_slotprocess();
+        break;
+    case 2:
+        recordprocess();
+        break;
+    case 3:
+        infoprocess();
+        break;
+    case 4:
+        cur_screen = Screen::end;
+        break;
+    default:
+        cout<<"Invalid Choice"<<endl;
+        break;
+    }
 }    
  
+void save_slotprocess()
         
         
         
         
-        
-        return 0;
-}
+
+
