035569b746500a8d181ace86eb27f8080ec53bc2
ZFSR <139683209+ZFSR@users.noreply.github.com>
2026-04-29 00:18:58 +0800

    fixed

--- map.cpp
+++ map.cpp
@@ -138,7 +138,7 @@ void displayNodeType(NodeType type) {
     }
 }
 
-//this function↓ is only used for test the random system. will not be contained in final code (We will make it be showed by the map_screen)
+//this function↓ is only used for testing the random system. will not be contained in final code (We will make it be showed by the map_screen)
 void playMap() {
     Map map(5);
 
