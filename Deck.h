6c9d93e321f86d894ce2783bf08ad9343644ceb4
Zhang Haobin <zhanghaobin2007@gmail.com>
2026-04-28 10:19:02 +0800

    Create Deck.h

---
+++ Deck.h
@@ -0,0 +1,12 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <vector>
+#include "Card.h"
+
+using namespace std;
+
+void printDeck(const vector<Card>& deck);
+void shuffleDeck(vector<Card>& deck);
+
+#endif
