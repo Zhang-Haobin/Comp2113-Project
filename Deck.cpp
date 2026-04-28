5c9091741bd235d6c74575b75474174079672a84
Zhang Haobin <zhanghaobin2007@gmail.com>
2026-04-28 10:18:18 +0800

    Create Deck.cpp

---
+++ Deck.cpp
@@ -0,0 +1,20 @@
+#include "Deck.h"
+
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+void printDeck(const vector<Card>& deck) {          //This function print the unshuffled card
+    for (int i = 0; i < deck.size(); i++) {
+        cout << i + 1 << ". " << deck[i].getName() << endl;
+    }
+}
+
+void shuffleDeck(vector<Card>& deck) {          //This function print the shuffled card
+    for (int i = deck.size() - 1; i > 0; i--) { 
+        int random_index = rand() % (i + 1);
+        swap(deck[i], deck[random_index]);
+    }
+}
