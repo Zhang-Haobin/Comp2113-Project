630364579116ecfc012aa15cda74b52ff5d670ae
ZFSR <139683209+ZFSR@users.noreply.github.com>
2026-04-28 23:25:24 +0800

    Create map.h

---
+++ map.h
@@ -0,0 +1,42 @@
+#ifndef MAP_H
+#define MAP_H
+
+#include <vector>
+#include <utility>  
+
+enum class NodeType {
+    NormalEnemy,
+    Event,
+    Boss,
+    Start
+};
+
+struct Node {
+    NodeType type;
+    std::vector<int> nextIndices;
+};
+
+class Map {
+public:
+    std::vector<std::vector<Node>> layers;
+    int currentLayer;
+    int currentNodeIdx;
+
+    Map(int numLayers = 5);
+
+    void generate();
+
+    Node& getCurrentNode();
+
+    std::vector<std::pair<int, NodeType>> getNextNodes();
+
+    bool moveToNextLayer(int nextNodeIdx);
+
+    bool isBossLayer();
+};
+
+void displayNodeType(NodeType type);
+
+void playMap();
+
+#endif 
