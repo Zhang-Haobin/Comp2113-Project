0d0f0aaf0c6285f5d5cfbc13c4df04fd8899eaf0
ZFSR <139683209+ZFSR@users.noreply.github.com>
2026-04-29 18:51:36 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -70,7 +70,46 @@ void save_slot_screen() {
 
 void map_screen() {
     Map map(difficulty*4+10)
-    
+     while (!map.isBossLayer()) {
+        cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"; 
+       
+
+        Node& cur = map.getCurrentNode();
+        cout << "=== Current Stage ===" << endl;
+        cout << "           " << map.currentLayer << endl;
+        displayNodeType(cur.type);
+        cout << "\n\n";
+
+        auto nexts = map.getNextNodes();
+
+        cout<<"Reachable Nodes:"<<endl;
+        for (size_t i = 0; i < nexts.size(); ++i) {
+            cout << "  " << (i + 1) << ". ";
+            displayNodeType(nexts[i].second);
+            cout << "\n";
+        }
+
+        cout << "\nEnter (1-" << nexts.size() << ") to move or enter 0 to pause: ";
+        int choice;
+        cin >> choice;
+
+        if (choice == 0) {
+            cout <<" Game Paused "<< endl;
+
+            return;
+        }
+
+        if (choice >= 1 && choice <= static_cast<int>(nexts.size())) {
+            int targetIdx = nexts[choice - 1].first;
+            if (map.moveToNextLayer(targetIdx)) {
+                continue; 
+            }
+        }
+
+        cout << "Invalid choice\n";
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin.get();
+    }
 }
 
 void battle_screen() {
