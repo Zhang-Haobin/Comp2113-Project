35354cb42326d73cd3b3db1a4b4c0c4c7e4d1158
dyn-p <dynpallomah@gmail.com>
2026-04-29 22:33:13 +0800

    fix

--- battle.cpp
+++ battle.cpp
@@ -9,53 +9,100 @@
 
 using namespace std;
 
+void Battle::print_and_select_options() {
+    cout << player.name << " HP " << player.hp << "/" << player.max_hp << " | Block 0 | Energy " << player.energy << " | Potions 0\n\n";
+
+    if(enemies.empty()) {
+        cout << "(Silence... No enemies present)\n\n";
+    }
+    else {
+        for(const Enemy &enemy : enemies) {
+            cout << enemy.name << " HP " << enemy.hp << "/" << enemy.max_hp << " | Next attack " << enemy.attack << "\n";
+        }
+        cout << "\n";
+    }
+
+    cout << "Hand:\n\n";
+    valid_options.clear();
+    int option_name = 1;
+
+    if(player.cards.empty()) {
+        cout << "(You don't have any cards)\n";
+    }
+    else {
+        for(const Card &card : player.cards) {
+            cout << option_name << ". \n\n"; // todo
+            valid_options[to_string(option_name)] = [&]() {
+                if(!card.isPlayable(player.energy)) {
+                    cout << "Not enough energy to play " << card.getName() << "!\n";
+                    return;
+                }
+                player.energy -= card.getCost();
+
+                cout << "You played " << card.getName() << "!\n";
+                round = BattleRound::option_result;
+                // todo: implement card effects
+            };
+            ++option_name;
+        }
+    }
+    cout << option_name << ". End turn\n\n";
+    valid_options[to_string(option_name)] = [&]() {
+        // end turn: do nothing
+        round = BattleRound::option_result;
+    };
+    ++option_name;
+
+    cout << option_name << ". Drink potion\n\n";
+    valid_options[to_string(option_name)] = [&]() {
+        // todo: does not have potion currently
+        round = BattleRound::option_result;
+    };
+}
+
+void Battle::print_option_result() const {
+    cout << "You performed" << 123 /* todo */ << "!" << "\n";
+    cout << "Press enter to continue...\n";
+}
+
 void Battle::print_battle_screen() {
-	cout << player.name << " HP " << player.hp << "/" << player.max_hp << " | Block 0 | Energy " << player.energy << " | Potions 0\n\n";
-
-	if(enemies.empty()) {
-		cout << "(Silence... No enemies present)\n\n";
-	}
-	else {
-		for(const Enemy &enemy : enemies) {
-			cout << enemy.name << " HP " << enemy.hp << "/" << enemy.max_hp << " | Next attack " << enemy.attack << "\n";
-		}
-		cout << "\n";
-	}
-
-	cout << "Hand:\n\n";
-	valid_options.clear();
-	int option_name = 1;
-
-	if(player.cards.empty()) {
-		cout << "(You don't have any cards)\n";
-	}
-	else {
-		for(const Card &card : player.cards) {
-			cout << option_name << ". \n\n"; // todo
-			valid_options.push_back(to_string(option_name));
-			++option_name;
-		}
-	}
-	cout << option_name << ". End turn\n\n";
-	valid_options.push_back(to_string(option_name));
-	++option_name;
-
-	cout << option_name << ". Drink potion\n\n";
-	valid_options.push_back(to_string(option_name));
+    switch(round) {
+    case BattleRound::select_option: {
+        print_and_select_options();
+        break;
+    }
+    case BattleRound::option_result: {
+        print_option_result();
+        break;
+    }
+    default: {
+        cout << "Unimplemented battle round!\n";
+        break;
+    }
+    }
 }
 
 void Battle::process_player_input() {
-	// todo: save the current battle state every turn
+    // todo: save the current battle state every turn
+    switch(round) {
+    case BattleRound::select_option: {
+        string option;
+        cin >> option;
 
-	string option;
-	cin >> option;
+        if(valid_options.find(option) == valid_options.end()) { // can't find this player-input option
+            cout << "'" << option << "' is not a valid option!\n";
+            return;
+        }
 
-	auto option_it = find(valid_options.begin(), valid_options.end(), option); // iterator; typename too complex
-	if(option_it != valid_options.end()) { // COULDN'T find this player-input option
-		cout << "'" << option << "' is not a valid option!\n";
-		return;
-	}
-	// else:
-
-	; 
+        // else run the option function
+        valid_options[option]();
+        break;
+    }
+    case BattleRound::option_result: {
+        string input;
+        cin >> input;
+        round = BattleRound::select_option;
+        break;
+    }
+    }
 }
--- battle.h
+++ battle.h
@@ -2,22 +2,36 @@
 #define BATTLE_H
 
 #include <string>
-#include <vector>
+#include <functional>
+#include <unordered_map>
 
 #include "player.h"
 #include "enemy.h"
 
+template <typename K, typename V>
+using umap = unordered_map<K, V>;
+
 // a "screen", initialized by the main loop
+
+enum class BattleRound {
+    select_option,
+    option_result,
+};
 
 class Battle {
 public:
-	Player player;
-	vector<Enemy> enemies;
+    Player player;
+    vector<Enemy> enemies;
+    BattleRound round = BattleRound::select_option;
 
-	vector<string> valid_options;
+    umap<string, function<void()>> valid_options;
 
-	void print_battle_screen();
-	void process_player_input();
+    Battle() = default;
+
+    void print_and_select_options();
+    void print_option_result() const;
+    void print_battle_screen();
+    void process_player_input();
 };
 
 #endif // ifndef BATTLE_H
