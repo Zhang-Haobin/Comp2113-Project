7668f0f72e9706ec43be492f97ff12ee24f5fff9
Zhang Haobin <zhanghaobin2007@gmail.com>
2026-04-29 21:33:52 +0800

    Update save.h

--- save.h
+++ save.h
@@ -3,23 +3,33 @@
 
 using namespace std;
 
-struct Record {
+struct record{
+
     int best_score = 0;
     int highest_floor = 0;
     int total_wins = 0;
     int total_losses = 0;
 
     void reset_record();
+    
     int get_total_game();
+
     double win_rate();
 };
 
-Record get_record();
-bool save_record(const Record& record);
-void update_record(int current_score, int current_floor, bool player_win);
+
+
+
+record get_record();
+
+bool save_record(const record& record);
+
+void update_record(int current_score, int current_floor,bool player_win);
+
 void print_record();
 
-// todo: maybe add a current battle state save
-//       if enough time
 
-#endif // ifndef SAVE_H
+
+
+
+#endif 
