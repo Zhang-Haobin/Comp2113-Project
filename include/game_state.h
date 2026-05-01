f1134182da4e0db817774b1f83624a2e8766f47c
TinaZhang0424 <u3312009@connect.hku.hk>
2026-05-01 21:47:29 +0800

    Create game_state.h

---
+++ include/game_state.h
@@ -0,0 +1,24 @@
+#ifndef GAME_STATE_H
+#define GAME_STATE_H
+
+#include "player.h"
+#include "map.h"
+
+using namespace std;
+
+struct GameState {
+    Player player;
+    Map* current_map;
+    int score;
+    int current_floor;
+    bool is_boss_fight;
+    
+    GameState();
+    ~GameState();
+    
+    void save_to_file(const string& filename);   // Write to file
+    bool load_from_file(const string& filename); // Read from file
+    void reset();                                 // Clear all data
+};
+
+#endif
